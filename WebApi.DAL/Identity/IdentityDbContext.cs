55c4efd2674df05cae30afb43c1a6166becf023e
Bohdan Dokhniak <bogdan270398@gmail.com>
2018-08-15 11:28:19 +0300

    Commit with configured migrations

---
+++ WebApi.DAL/Identity/IdentityDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Entities;
+
+namespace WebApi.Identity
+{
+    public class UserIdentityDbContext : IdentityDbContext
+    {
+        private readonly IConfiguration _config;
+
+        public UserIdentityDbContext(IConfiguration config, DbContextOptions<UserIdentityDbContext> options)
+                : base(options)
+        {
+            _config = config ?? throw new System.ArgumentNullException(nameof(config));
+        }
+
+        public DbSet<IdentityUser> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"), options =>
+            {
+                options.MigrationsHistoryTable("UsersMigrationsHistory", "WebApi");
+            });
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ WebApi.DAL/Migrations/20180815073327_Second.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.DAL.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "TaskItem",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Value = table.Column<string>(maxLength: 60, nullable: true),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskItem", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskItem",
+                schema: "dbo");
+        }
+    }
+}
